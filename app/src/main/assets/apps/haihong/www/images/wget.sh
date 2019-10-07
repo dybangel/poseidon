de973be6bc4a5154a88064d9097fd9ce2d2220e2
dybangel <176682769@qq.com>
2019-10-08 00:53:09 +0800

    del file

--- app/src/main/assets/apps/haihong/www/images/wget.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cat file.txt|while read url 
-do
-wget $url
-done
--- app/src/main/java/com/haihong/dianqu/wxapi/WXPayEntryActivity.java
+++
@@ -1,7 +0,0 @@
-package com.haihong.dianqu.wxapi;
-
-import io.dcloud.feature.payment.weixin.AbsWXPayCallbackActivity;
-
-public class WXPayEntryActivity extends AbsWXPayCallbackActivity{
-
-}
